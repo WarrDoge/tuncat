f23024ba674aa0c8591b37e974764d25b8415048
ddudko <ddudko@localhost>
2026-03-20 01:21:41 +0200

    Bump version to v0.2.1

--- internal/cli/constants.go
+++ internal/cli/constants.go
@@ -3,7 +3,7 @@ package cli
 import "time"
 
 const (
-	Version = "0.2.0"
+	Version = "0.2.1"
 
 	obscureKey    = "\x9c\x93\x5b\x48\x73\x0a\x55\x4d\x6b\xe0\x76\x3f\x1a\xc4\xd8\x2e"
 	obscurePrefix = "obscured:"
